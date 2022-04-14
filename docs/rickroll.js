4aab68da481cf30a7953f98ed429a7f284773b13
Raspberry_Pi_Lincoln <MrSmartyMcFarty@gmail.com>
2022-04-14 17:53:05 -0500

    fixed rickroll

--- docs/rickroll.js
+++ docs/rickroll.js
@@ -1,5 +1,5 @@
 var random = Math.round(Math.random() * 9) + 1;
 console.log(random);
 if (random == 1) {
-  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ.";
+  window.location.href = "rickroll.html";
 }
